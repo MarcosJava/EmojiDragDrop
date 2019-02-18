a2a6ab359bced26976b26905dbc665f804563d4b
Marcos Felipe Souza <mfelipesp@gmail.com>
2019-02-18 00:07:08 -0300

    finished 13 classrom

---
+++ EmojiDragDrop/EmojiArt.swift
@@ -0,0 +1,9 @@
+//
+//  EmojiArt.swift
+//  EmojiDragDrop
+//
+//  Created by Marcos Felipe Souza on 17/02/19.
+//  Copyright © 2019 Marcos. All rights reserved.
+//
+
+import Foundation
---
+++ EmojiDragDrop/TextFieldCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  TextFieldCollectionViewCell.swift
+//  EmojiDragDrop
+//
+//  Created by Marcos Felipe Souza on 16/02/19.
+//  Copyright © 2019 Marcos. All rights reserved.
+//
+
+import UIKit
+
+class TextFieldCollectionViewCell: UICollectionViewCell {
+    
+}
